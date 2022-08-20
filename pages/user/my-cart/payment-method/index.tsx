0bb3b14318a753271b759851732ddcf85a2df3fe
Amitav Roy <reachme@amitavroy.com>
2022-08-20 11:51:03 +0530

    Forgot this file

--- pages/user/my-cart/payment-method/index.tsx
+++ pages/user/my-cart/payment-method/index.tsx
@@ -1,7 +1,14 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 import { useState } from "react";
+
 import Layout from "../../../../components/Layout";
+import { activeOrderQuery } from "../../../../queries/common/active-order.query";
 import { availablePaymentMethodsQuery } from "../../../../queries/common/available-payment.query";
+import { eligibleShippingMethodsQuery } from "../../../../queries/common/shipping-methods.query";
+import { addPaymentToOrderMutation } from "../../../../queries/mutations/add-payment-to-order.mutation";
+import { changeOrderStateMutation } from "../../../../queries/mutations/change-order-state.mutation";
+import { setOrderShippingMethod } from "../../../../queries/mutations/set-shipping-method.mutation";
 
 interface IData {
   eligiblePaymentMethods: Array<{
@@ -13,30 +20,94 @@ interface IData {
   }>;
 }
 
-interface IOrderStages {
-  selectedAddress: boolean;
-  selectedPaymentMethod: boolean;
+interface IShippingData {
+  eligibleShippingMethods: Array<{
+    id: number;
+    name: string;
+    code: string;
+    price: string;
+    priceWithTax: string;
+  }>;
 }
 
 const PaymentMethod = () => {
+  const router = useRouter();
   const { data, loading, error } = useQuery<IData>(
     availablePaymentMethodsQuery
   );
-  const [stages, setStages] = useState<IOrderStages>({
-    selectedAddress: false,
-    selectedPaymentMethod: false,
-  });
-
+  const {
+    data: shippingData,
+    loading: shippingLoading,
+    error: shipError,
+  } = useQuery<IShippingData>(eligibleShippingMethodsQuery);
+  const [addPaymentMethod] = useMutation(addPaymentToOrderMutation);
+  const [addShippingMethod] = useMutation(setOrderShippingMethod);
+  const [changeOrderState] = useMutation(changeOrderStateMutation);
+  const handleMethodSelection = (type: string, value: string) => {
+    switch (type) {
+      case "shipping":
+        addShippingMethod({ variables: { id: value } });
+        break;
+      case "payment":
+        addPaymentMethod({
+          variables: { method: value, metaData: {} },
+        });
+        break;
+      default:
+        break;
+    }
+  };
+  const makePayment = async (state: "ArrangingPayment" | "PaymentSettled") => {
+    await changeOrderState({ variables: { state } });
+    router.push("/user/my-cart/payment-method");
+  };
   return (
     <Layout pageTitle="Select payment method">
-      {!loading && !error && (
+      {!shippingLoading && !shipError && (
         <div>
+          <p>Select shipping method from the below list:</p>
+          <ul>
+            {shippingData?.eligibleShippingMethods.map((method) => {
+              return (
+                <li key={method.id}>
+                  <h2
+                    className="bold text-2xl cursor-pointer"
+                    onClick={() =>
+                      handleMethodSelection("shipping", method.id.toString())
+                    }
+                  >
+                    {method.name}
+                  </h2>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+
+      <div className="mt-10">
+        <p>Click here to make payment for your order</p>
+        <h2
+          className="bold text-2xl cursor-pointer"
+          onClick={() => makePayment("ArrangingPayment")}
+        >
+          Make payment
+        </h2>
+      </div>
+
+      {!loading && !error && (
+        <div className="mt-10">
           <p>Select payment method from the below list:</p>
           <ul>
             {data?.eligiblePaymentMethods.map((method) => {
               return (
                 <li key={method.id}>
-                  <h2 className="bold text-2xl cursor-pointer">
+                  <h2
+                    className="bold text-2xl cursor-pointer"
+                    onClick={() =>
+                      handleMethodSelection("payment", method.code)
+                    }
+                  >
                     {method.name}
                   </h2>
                   <small>{method.description}</small>
